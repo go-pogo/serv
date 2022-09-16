ad0b0da1c3eb2388842fb2f665400e0ad5622c05
roeldev <roeldev@users.noreply.github.com>
2022-09-17 00:15:52 +0200

    Add http headers

---
+++ httpheader/header.go
@@ -0,0 +1,16 @@
+// Copyright (c) 2022, Roel Schut. All rights reserved.
+// applyOptions of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package httpheader
+
+const (
+	AccessControlAllowCredential = "Access-Control-Allow-Credential"
+	AccessControlAllowHeaders    = "Access-Control-Allow-Headers"
+	AccessControlAllowMethods    = "Access-Control-Allow-Methods"
+	AccessControlAllowOrigin     = "Access-Control-Allow-Origin"
+	AccessControlExposeHeaders   = "Access-Control-Expose-Headers"
+	AccessControlMaxAge          = "Access-Control-Max-Age"
+
+	WWWAuthenticate = "WWW-Authenticate"
+)
