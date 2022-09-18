fdfe42c3b8ccb263e3fbbc48a024e910de5c5ea2
roeldev <roeldev@users.noreply.github.com>
2022-09-18 16:00:00 +0200

    Fix license notice

--- cors/cors.go
+++ cors/cors.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2022, Roel Schut. All rights reserved.
-// applyOptions of this source code is governed by a BSD-style
+// Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package cors
