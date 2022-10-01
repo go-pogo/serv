f7a89e1d744ffbce339fa14f9efc20386f6f4bc0
roeldev <roeldev@users.noreply.github.com>
2022-10-01 15:21:55 +0200

    Add request logger

--- log.go
+++ log.go
@@ -6,10 +6,19 @@ package serv
 
 const panicNilLogger = "Logger should not be nil"
 
+type Logger interface {
+	ServerLogger
+	RouterLogger
+}
+
 type ServerLogger interface {
 	LogServerStart(addr string)
 	LogServerShutdown()
 	LogServerClose()
+}
+
+type RouterLogger interface {
+	LogRegisterRoute(method, path string)
 }
 
 func WithLogger(l ServerLogger) Option {
@@ -22,10 +31,11 @@ func WithLogger(l ServerLogger) Option {
 	})
 }
 
-func NopLogger() ServerLogger { return new(nopLogger) }
+func NopLogger() Logger { return new(nopLogger) }
 
 type nopLogger struct{}
 
-func (_ *nopLogger) LogServerStart(_ string) {}
-func (_ *nopLogger) LogServerShutdown()      {}
-func (_ *nopLogger) LogServerClose()         {}
+func (_ *nopLogger) LogServerStart(_ string)      {}
+func (_ *nopLogger) LogServerShutdown()           {}
+func (_ *nopLogger) LogServerClose()              {}
+func (_ *nopLogger) LogRegisterRoute(_, _ string) {}
