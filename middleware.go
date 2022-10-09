11441ec23695d9b1dff02520f10b8a227b93c27b
roeldev <roeldev@users.noreply.github.com>
2022-10-09 14:22:22 +0200

    Update middleware

--- middleware.go
+++ middleware.go
@@ -5,10 +5,13 @@
 package serv
 
 import (
+	"context"
 	"net/http"
 	"strings"
 )
 
+// Middleware wraps a http.Handler with additional logic and returns a new
+// http.Handler that executes it.
 type Middleware interface {
 	Wrap(next http.Handler) http.Handler
 }
@@ -25,6 +28,21 @@ func Wrap(h http.Handler, mw ...Middleware) http.Handler {
 	return h
 }
 
+func WithContextValue(key, value interface{}, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(wri http.ResponseWriter, req *http.Request) {
+		next.ServeHTTP(
+			wri,
+			req.WithContext(context.WithValue(req.Context(), key, value)),
+		)
+	})
+}
+
+func WithContextValueM(key, value interface{}) Middleware {
+	return MiddlewareFunc(func(next http.Handler) http.Handler {
+		return WithContextValue(key, value, next)
+	})
+}
+
 // RemoveTrailingSlash adds middleware that redirects any request which has a
 // trailing slash to the equivalent path without trailing slash.
 // It uses the redirect code http.StatusMovedPermanently for any GET request
@@ -39,20 +57,24 @@ func RemoveTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
+func RemoveTrailingSlashM() Middleware { return MiddlewareFunc(RemoveTrailingSlash) }
+
 // RedirectHttps adds middleware that redirects any http request to its https
 // equivalent url.
 // It uses the redirect code http.StatusMovedPermanently for any GET request
 // and http.StatusTemporaryRedirect for any other method.
-func RedirectHttps(next http.Handler) http.HandlerFunc {
-	return func(wri http.ResponseWriter, req *http.Request) {
+func RedirectHttps(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(wri http.ResponseWriter, req *http.Request) {
 		if req.URL.Scheme == "http" {
 			req.URL.Scheme += "s"
 			http.Redirect(wri, req, req.URL.String(), statusCode(req.Method))
 		} else {
 			next.ServeHTTP(wri, req)
 		}
-	}
+	})
 }
+
+func RedirectHttpsM() Middleware { return MiddlewareFunc(RedirectHttps) }
 
 func statusCode(method string) int {
 	switch method {
