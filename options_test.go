a993e7ff963367aa8894dbad19f72676d09178cb
roeldev <roeldev@users.noreply.github.com>
2024-03-03 14:49:11 +0100

    Update tests

--- options_test.go
+++ options_test.go
@@ -14,10 +14,4 @@ func TestServerName(t *testing.T) {
 	t.Run("empty value", func(t *testing.T) {
 		assert.Equal(t, "", ServerName(context.Background()))
 	})
-
-	t.Run("value", func(t *testing.T) {
-		want := "foobar"
-		ctx := context.WithValue(context.Background(), serverNameKey{}, want)
-		assert.Equal(t, want, ServerName(ctx))
-	})
 }
