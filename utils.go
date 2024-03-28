638d1dbdaf35613336774fe05faaf57763b30161
roeldev <roeldev@users.noreply.github.com>
2024-03-28 22:25:11 +0100

    Add WriteJSONError

--- utils.go
+++ utils.go
@@ -14,10 +14,25 @@ const contentTypeJSON = "application/json"
 
 // WriteJSON encodes v to JSON and writes it to w.
 func WriteJSON(w http.ResponseWriter, v any) error {
+	if v == nil {
+		return nil
+	}
+
 	if err := json.NewEncoder(w).Encode(v); err != nil {
 		return errors.WithStack(err)
 	}
 
 	w.Header().Set("Content-Type", contentTypeJSON)
 	return nil
+}
+
+func WriteJSONError(w http.ResponseWriter, err error) error {
+	if err == nil {
+		return nil
+	}
+
+	w.WriteHeader(errors.GetStatusCodeOr(err, http.StatusInternalServerError))
+	return WriteJSON(w, struct {
+		Error string `json:"error"`
+	}{Error: err.Error()})
 }
