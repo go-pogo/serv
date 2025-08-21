83eeb64c9fd07d238523c16a93db65cd6c4913ad
roeldev <roeldev@users.noreply.github.com>
2025-08-21 14:30:57 +0200

    Add cors sub pkg

---
+++ cors/cors_unsafe.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build cors_unsafe
+
+package cors
+
+const corsUnsafe = true
